ac21b68ba2c20100106323115696bfd8d5aa745d
admin <kangjianhao19@163.com>
2019-11-07 20:28:07 +0800

    test

---
+++ springboot01/src/main/java/com/itheima/springboot01/HELLO.java
@@ -0,0 +1,9 @@
+package com.itheima.springboot01;
+
+public class HELLO {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+        System.out.println("aaaaa");
+        System.out.println("aaaaa");
+    }
+}
